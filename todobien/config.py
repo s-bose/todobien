cf3d0590e8856643761ae1ffbf4ffee8eec2ecdd
shiladitya <shiladitya_basu@live.com>
2023-04-24 21:49:52 +0530

    remove poetry

--- todobien/config.py
+++ todobien/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    SQLITE_DB_PATH: Path = Path.home() / ".todobien" / "main.db"
+    SQLITE_DB_PATH: Path = Path.home() / ".todobien" / "main.sqlite"
     SQLITE_DB_STR: str = f"sqlite:///{str(SQLITE_DB_PATH)}"
 
 
