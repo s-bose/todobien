79a696dfdee3a6ca08d817cf98aa348603e373c5
shiladitya <shiladitya_basu@live.com>
2023-04-27 12:04:54 +0530

    testing cli interfaces

--- test.py
+++ test.py
@@ -1,6 +1,37 @@
-import sqlite3
-from todobien.config import settings
-import os
+import questionary
+import argparse
 
+projects = {
+    "todobien": [
+        "TDB-001",
+        "TDB-002",
+        "TDB-003",
+        "TDB-004",
+    ],
+    "fermit": [
+        "FMT-001",
+        "FMT-002",
+        "FMT-003",
+    ],
+    "random": [
+        "RND-42",
+        "RND-44",
+    ],
+}
 
-cursor = sqlite3.connect(str(settings.SQLITE_DB_PATH))
+argparser = argparse.ArgumentParser()
+
+argparser.add_argument("-p", "--path", required=False)
+
+if __name__ == "__main__":
+    args = argparser.parse_args()
+    resp = None
+    if not args.path:
+        resp = questionary.autocomplete(
+            "select project: ", choices=projects.keys()
+        ).ask()
+    resp = questionary.autocomplete(
+        "select task: ", choices=projects.get(resp or args.path)
+    ).ask()
+
+    print(resp)
--- todobien/config.py
+++ todobien/config.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 from pydantic import BaseSettings
+from pathlib import Path
 
 
 class Settings(BaseSettings):
     SQLITE_DB_PATH: Path = Path.home() / ".todobien" / "main.sqlite"
     SQLITE_DB_STR: str = f"sqlite:///{str(SQLITE_DB_PATH)}"
+    TODOBIEN_CONFIG_DIR = Path.home() / ".todobien"
 
 
 settings = Settings()
